8a008c57c360d3dd0378135851c48039c3e402f1
Timofey Basuev <echiauta@gmail.com>
2024-07-11 13:17:31 +0500

    fix: minor changes

--- storages/app/crud/products.py
+++
@@ -1,7 +0,0 @@
-from app.crud.base import CRUDBase
-from app.models.models import Product
-from app.models.models import ProductCreate
-from app.models.models import ProductUpdate
-
-CRUDProduct = CRUDBase[Product, ProductCreate, ProductUpdate]
-crud_product = CRUDProduct(Product)
--- storages/app/crud/storages.py
+++
@@ -1,7 +0,0 @@
-from app.crud.base import CRUDBase
-from app.models.models import Storage
-from app.models.models import StorageCreate
-from app.models.models import StorageUpdate
-
-CRUDStorage = CRUDBase[Storage, StorageCreate, StorageUpdate]
-crud_storage = CRUDStorage(Storage)
