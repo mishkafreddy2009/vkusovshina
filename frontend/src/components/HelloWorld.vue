db2d50a83eb3154e26c03f680345a3590b2cf6ea
Timofey Basuev <echiauta@gmail.com>
2024-07-11 13:16:27 +0500

    feat: create vue project

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'PingPong',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      axios.get('/api/v1/storages')
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
